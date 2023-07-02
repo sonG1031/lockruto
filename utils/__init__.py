3cd716ef133ff47fbe8c8f9b6a95a900426fff30
송민섭 <minsub1031@gmail.com>
2023-07-02 19:51:53 +0900

    basic jutsu detection settings

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from utils.playSoundEffect import playFinishedEffect, playEffect
+# __init__.py 에 위처럼 참조를 해주면
+# 외부에서 from utils import playEffect로 참조가 가능하다.
---
+++ utils/playSoundEffect.py
@@ -0,0 +1,8 @@
+from playsound import playsound
+import threading
+
+def playFinishedEffect():
+    threading.Thread(target=playsound, args=("./utils/sounds/jutsuFinished.mp3",), daemon=True).start()
+
+def playEffect():
+    threading.Thread(target=playsound, args=("./utils/sounds/jutsu.mp3",), daemon=True).start()
